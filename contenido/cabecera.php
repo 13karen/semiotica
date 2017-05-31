fe503785b5979de6cbca6d2ffe2e6b13dbd11b2c
13karen <13karenlc@gmail.com>
2017-05-31 03:02:44 -0500

    Update cabecera.php

--- contenido/cabecera.php
+++ contenido/cabecera.php
@@ -81,7 +81,7 @@
   <ul class="nav nav-tabs">
     <li role="presentation" class="active"><a href="index.php">Inicio</a></li>
     <li role="presentation" class="dropdown">
-      <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
         Semiótica <span class="caret"></span>
       </a>
       <ul class="dropdown-menu">
