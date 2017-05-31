1e8080ae2c444099019c528c7512ffb20fba4866
13karen <13karenlc@gmail.com>
2017-05-31 03:16:27 -0500

    Update ramas.php

--- ramas.php
+++ ramas.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Semiotica - Ramas</title>
-    <link href="/Semiotica/css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
 
 
     <style>
@@ -64,6 +64,5 @@
     <!--Pie de pagina -->
     <?php include('contenido/pie.php'); ?>
     <!-- -->
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="/Semiotica/js/bootstrap.min.js"></script>
+</body>
+  </html>
