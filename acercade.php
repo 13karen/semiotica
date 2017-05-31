bb02e559b2708d67f22ac4eb3ea373e49577f9eb
13karen <13karenlc@gmail.com>
2017-05-31 03:05:23 -0500

    Update acercade.php

--- acercade.php
+++ acercade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Semiotica - Acerca de</title>
-    <link href="/Semiotica/css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
   </head>
   <body>
     <!--Encabezado -->
